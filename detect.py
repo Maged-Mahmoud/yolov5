4c4f6b12553a8766973d6fc78ffe72eb8134c160
Maged Mahmoud <65985410+Maged-Mahmoud@users.noreply.github.com>
2022-05-30 01:24:18 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -44,7 +44,7 @@ from utils.general import (LOGGER, check_file, check_img_size, check_imshow, che
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
-
+import streamlit as st
 
 @torch.no_grad()
 def run(
