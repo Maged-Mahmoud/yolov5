36b0d2f40001c2a70295c145c13814698daaac5b
Maged Mahmoud <65985410+Maged-Mahmoud@users.noreply.github.com>
2022-05-31 00:58:22 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -266,7 +266,7 @@ def main(opt):
     
     st.title('Welcome to Drill Bit Failure Detection Project! V 1.0 DEMO')
     st.subheader('Presented to upstream oil and gas industry: Upload images and then Detect the type of failure then define the root cause of failure')
-    st.set_page_config(page_title = 'Drill Bit Failure Detection', page_icon = cv2.imread('images.jpg'))
+    st.set_page_config(page_title = 'Drill Bit Failure Detection')
     hide_menu_style = '''
     <style>
     # MainMenu{visibility: hidden;}
